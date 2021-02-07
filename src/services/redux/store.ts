acb9b72ccfb1eb90e4af19df802c7865c32f6b05
Omid Mokhtari <omidmokhtari93@gmail.com>
2021-02-07 21:56:35 +0330

    redux development tools added

--- src/services/redux/store.ts
+++ src/services/redux/store.ts
@@ -1,8 +1,10 @@
-import { combineReducers, createStore } from "redux";
+import { applyMiddleware, combineReducers, createStore } from "redux";
 import { CartReducer } from "./reducers/cartReducer";
+import { composeWithDevTools } from "redux-devtools-extension/developmentOnly";
+import thunk, { ThunkMiddleware } from "redux-thunk";
 
 const rootReducr = combineReducers({
     cart: CartReducer
 })
 
-export const store = createStore(rootReducr);
+export const store = createStore(rootReducr, composeWithDevTools(applyMiddleware(thunk)));
