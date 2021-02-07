3b95a799d872f7e6424f999be551f92ad2271e63
Omid Mokhtari <omidmokhtari93@gmail.com>
2021-02-07 23:32:39 +0330

    dispatch action added

--- src/services/redux/store.ts
+++ src/services/redux/store.ts
@@ -1,7 +1,12 @@
-import { applyMiddleware, combineReducers, createStore } from "redux";
+import { Action, applyMiddleware, combineReducers, createStore } from "redux";
 import { CartReducer } from "./reducers/cartReducer";
 import { composeWithDevTools } from "redux-devtools-extension/developmentOnly";
-import thunk, { ThunkMiddleware } from "redux-thunk";
+import thunk from "redux-thunk";
+import { rootState } from ".";
+
+export interface dispatchAction extends Action {
+    payload: rootState
+}
 
 const rootReducr = combineReducers({
     cart: CartReducer
