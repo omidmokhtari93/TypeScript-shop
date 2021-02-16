33edf46d6c38f7a4750950f8939fd143d57cf25b
Omid Mokhtari <omidmokhtari93@gmail.com>
2021-02-16 19:28:51 +0330

    router added to reducers

--- src/services/redux/index.ts
+++ src/services/redux/index.ts
@@ -1,10 +1,13 @@
+import { connectRouter } from "connected-react-router";
 import { combineReducers } from "redux";
+import browserHistory from "../browserHistory";
 import { cartReducer, cartState } from "../redux/reducers/cartReducer";
 
 export const rootReducr = combineReducers({
     cart: cartReducer,
+    router: connectRouter(browserHistory)
 });
 
-export interface rootState extends cartState {}
+export interface rootState extends cartState { }
 
 export type rootStateType = ReturnType<typeof rootReducr>;
